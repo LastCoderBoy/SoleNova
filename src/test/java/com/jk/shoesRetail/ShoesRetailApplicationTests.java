5f661ff559bda5c7a343c961c4851b7eadca1cc3
Jasurbek Khamroev <khamroevapple@gmail.com>
2026-03-10 10:02:31 +0100

    initial test commit

---
+++ src/test/java/com/jk/shoesRetail/ShoesRetailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jk.shoesRetail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoesRetailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
